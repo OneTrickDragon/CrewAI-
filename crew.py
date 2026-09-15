75f3c591956c42d5f7e41416f7092c01c0bb4e4b
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-09-15 23:35:46 +0530

    updated structure

--- crew.py
+++ crew.py
@@ -1,43 +1,23 @@
-from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
-from crewai.knowledge.source.pdf_knowledge_source import PDFKnowledgeSource
+from crewai import Crew
+
+from agents.research_specialist import research_specialist_agent
+from agents.data_analyst import data_analyst_agent
+from agents.content_writer import content_writer_agent
+from tasks.research_task import research_task
+from tasks.analysis_task import analysis_task
+from tasks.writing_task import writing_task
 
 
-pdf_source = PDFKnowledgeSource(
-    file_paths=["meta_quest_manual.pdf"]
+research_crew = Crew(
+    agents=[
+        research_specialist_agent,
+        data_analyst_agent,
+        content_writer_agent,
+    ],
+    tasks=[
+        research_task,
+        analysis_task,
+        writing_task,
+    ],
+    verbose=True
 )
-
-
-@CrewBase
-class MetaQuestKnowledge():
-	"""MetaQuestKnowledge crew"""
-
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
-
-	@agent
-	def meta_quest_expert(self) -> Agent:
-		return Agent(
-			config=self.agents_config['meta_quest_expert'],
-			verbose=True
-		)
-
-	@task
-	def answer_question_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['answer_question_task'],
-		)
-
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the MetaQuestKnowledge crew"""
-
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=True,
-			knowledge_sources=[
-				pdf_source
-			]
-		)
